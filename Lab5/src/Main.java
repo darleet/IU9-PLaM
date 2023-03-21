ea7c27190b2a93a2cbb3694ebd938532d298b3f6
darleet <darleet@mail.ru>
2023-03-21 23:41:19 +0300

    Added Lab5

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        Numbers myNumbers = new Numbers();
+
+        // Добавим в множество несколько чисел
+        myNumbers.addNumber(6);
+        myNumbers.addNumber(5);
+        myNumbers.addNumber(9);
+        myNumbers.addNumber(-1);
+        myNumbers.addNumber(-8);
+        myNumbers.addNumber(0);
+
+        // Выведем произведения по группам
+        System.out.println("Отрицательные произведения: " + myNumbers.getProds()
+                .filter(x -> x < 0)
+                .toList());
+        System.out.println("Нулевые произведения: " + myNumbers.getProds()
+                .filter(x -> x == 0)
+                .toList());
+        System.out.println("Положительные произведения: " + myNumbers.getProds()
+                .filter(x -> x > 0)
+                .toList());
+
+        // Получим число "X"
+        if (myNumbers.getX().isPresent()) {
+            System.out.println("Число \"X\": " + myNumbers.getX().get());
+        } else {
+            System.out.println("Число \"X\" не найдено!");
+        }
+
+    }
 }
--- Lab5/src/Numbers.java
+++ Lab5/src/Numbers.java
@@ -1,2 +1,26 @@
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Numbers {
+    private final HashSet<Integer> numbers = new HashSet<>();
+
+    public void addNumber(Integer number) {
+        this.numbers.add(number);
+    }
+
+    public Stream<Integer> getProds() {
+        HashSet<Integer> results = new HashSet<>();
+        numbers.forEach(x -> numbers.stream()
+                .parallel()
+                .filter(y -> !Objects.equals(y, x))
+                .forEach(y -> results.add(x * y)));
+        return results.stream();
+    }
+
+    public Optional<Integer> getX() {
+        return numbers.stream()
+                .filter(x -> numbers.stream().allMatch(y -> Objects.equals(y, x) ||
+                        Math.abs(y) < Math.abs(x)))
+                .findAny();
+    }
 }
